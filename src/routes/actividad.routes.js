9d4d385cadc0a0534e6806b34977362ec3137df7
Dussan01 <Dussan01@users.noreply.github.com>
2021-01-28 14:13:40 -0500

    Desarrollo modulo de subir actividades con sus carpetas respectivas y CRUD

--- src/routes/actividad.routes.js
+++ src/routes/actividad.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 const router = Router();
 import * as ActividadCtrl from "../controllers/actividad.controller";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 router.get('/:Grade&:Matter', ActividadCtrl.getActivity);
 router.post('/:Grade&:Matter', ActividadCtrl.createActivity);
@@ -9,4 +12,4 @@ router.get('/:name', ActividadCtrl.downloadActivity);
 router.get('/', ActividadCtrl.getActivity);
 
 
-export default router
+export default router;
